f1e10e46d4cf5cba543fecc526b052fe865a2680
nilshendriks <nilshendriks@me.com>
2023-05-03 16:03:39 +0200

    - removed astro plugin webmanifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import webmanifest from "astro-webmanifest";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [webmanifest({
-    /**
-     * required
-     **/
-    name: 'Studio HENK',
-    icon: 'src/images/your-icon.svg',
-  })]
+  integrations: []
 });
