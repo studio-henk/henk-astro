a56427fc2156cdb8810df19f1ee0e4c379dbc1cb
nilshendriks <nilshendriks@me.com>
2023-05-26 08:55:09 +0200

    - tested 2 languages with ability to have the other language for current page (like in pimcore)

---
+++ src/i18n/helpers.js
@@ -0,0 +1,12 @@
+export function getOtherLang(lang) {
+    let langIndex;
+    let otherLang;
+    if (lang == 'en') {
+        langIndex = 0;
+        otherLang = 1;
+    } else {
+        langIndex = 1;
+        otherLang = 0;
+    }
+    return otherLang;
+}
